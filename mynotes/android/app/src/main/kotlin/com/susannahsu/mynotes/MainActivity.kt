f4faa8b101dfa5693f93ebaafcda7e938b197d6f
Susannah Su <susannah_su@college.harvard.edu>
2022-03-14 11:50:45 -0400

    go thru the first few chap of tutorial; create mynotes project under domain

---
+++ mynotes/android/app/src/main/kotlin/com/susannahsu/mynotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.susannahsu.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -4,16 +4,12 @@ void main() {
   runApp(const MyApp());
 }
 
-
-void test() {}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    test();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
